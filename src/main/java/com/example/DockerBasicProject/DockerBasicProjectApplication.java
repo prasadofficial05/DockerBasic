cb53689f8d42041c529b602df1305a7f5a95e50e
Prasad <prasaddeore05@gmail.com>
2026-02-19 20:13:33 +0530

    add DockerBasicProject

---
+++ src/main/java/com/example/DockerBasicProject/DockerBasicProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.DockerBasicProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerBasicProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerBasicProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DockerBasicProject/DockerBasicProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DockerBasicProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerBasicProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
